3ac3590cdab9075d293f699c4c465e144358414e
montsegh4545 <80225989+montsegh4545@users.noreply.github.com>
2022-03-29 10:50:20 -0600

    nada sirve pero todo medio ayuda

---
+++ Association.java
@@ -0,0 +1,29 @@
+public class Association<K extends Comparable<K>, V> implements Comparable<Association<K,V>> {
+
+	K key;
+	V value;
+	
+	Association(K key_, V value_){
+		key = key_;
+		value = value_;
+	}
+	
+	public K getKey() {
+		return key;
+	}
+	
+	public V getValue() {
+		return value;
+	}
+	
+
+	public int compareTo(Association<K, V> o) {
+		return key.compareTo(o.getKey());
+	}
+
+    public String toString(){
+        return key.toString();
+    }
+
+
+}
